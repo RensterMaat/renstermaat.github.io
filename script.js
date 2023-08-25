b79274f98d7e82481d960eb7b40c931111e27d8d
RensterMaat <rens.termaat@gmail.com>
2023-08-25 16:18:14 +0200

    implement logreg

--- script.js
+++ script.js
@@ -28,12 +28,60 @@ function setCategory(attribute, category) {
     }
 }
 
-// Dummy function to calculate the probability
 function calculateProbability() {
-    // Here you can add your real logic to calculate the probability based on the selections
-    let randomProbability = Math.floor(Math.random() * 100);
-    document.getElementById('result').innerText = `${randomProbability}%`;
+    // Get values from your buttons or inputs
+    // ...
+
+    // Coefficients
+    const coefficients = {
+        'clarkScore': {
+            'brisk': 0.7057,
+            'non-brisk': 0.3649,
+            'missing': 0,
+            'absent': 0
+        },
+        'ldh': {
+            'elevated': 0.1873,
+            'moreThanTwice': -0.9471,
+            'missing': 0.5858,
+            'normal': 0
+        },
+        'whoStatus': {
+            '1': -0.1744,
+            '2-4': -1.2297,
+            'missing': -0.4334,
+            '0': 0
+        },
+        'brainMetastases': {
+            'presentAsymptomatic': -0.0575,
+            'missing': 0.2644,
+            'presentSymptomatic': -1.1120,
+            'absent': 0
+        },
+        'liverMetastases': {
+            'missing': -0.0451,
+            'present': -0.8500,
+            'absent': 0
+        }
+    };
+
+    let z = 1.155624; // Intercept
+
+    // Update the z value based on the input features
+    for (let attribute in selections) {
+        z += coefficients[attribute][selections[attribute]];
+    }
+
+    // Calculate the probability using the logistic function
+    let probability = 1 / (1 + Math.exp(-z));
+
+    // Convert to percentage for displaying
+    let percentage = Math.round(probability * 100);
+
+    document.getElementById("result").textContent = percentage + "%";
 }
+
+
 
 // Reset function
 function resetCalculator() {
